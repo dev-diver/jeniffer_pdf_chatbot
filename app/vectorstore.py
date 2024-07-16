3cc0bb6b8e1b888c043032b2e490f2d89829762c
SoKyungHyun <valentine92exp@gmail.com>
2024-07-16 15:20:57 +0900

    embedding docs -> split

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,11 +1,11 @@
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
-from document import docs
+from document import splits
 
 embeddings_model = HuggingFaceEmbeddings()
 vectorstore = Chroma.from_documents(
-    docs,
+    splits,
     embedding=embeddings_model,
 )
 print("embed complete")
