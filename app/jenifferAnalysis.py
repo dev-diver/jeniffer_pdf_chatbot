06595ec9d0aed4c71edbb652f8df0646a98bf933
SoKyungHyun <valentine92exp@gmail.com>
2024-07-18 14:52:20 +0900

    classify with size and fond

--- app/jenifferAnalysis.py
+++ app/jenifferAnalysis.py
@@ -1,5 +1,6 @@
 import fitz
 from pathlib import Path
+from collections import defaultdict
 
 current_dir = Path(__file__).parent
 project_dir = current_dir.parent
@@ -9,29 +10,38 @@ output_path = Path(data_dir, "output.txt")
 
 document = fitz.open(file_path)
 
-size_hash = set()
+files = {}
 
-sizes = [1.9800000190734863, 6.960000038146973, 7.019999980926514, 7.980000019073486, 9.0, 9.479999542236328, 9.960000038146973, 10.020000457763672, 12.0, 13.979999542236328, 14.399999618530273, 18.0, 19.979999542236328, 24.959999084472656, 30.0, 34.97999954223633, 45.0, 79.9800033569336]
-files = {x:{} for x in sizes}
-print(files)
-
-for i, page in enumerate(document):
+for page_num, page in enumerate(document):
     text_page = page.get_text("dict")
     text_blocks = []
     for block in text_page["blocks"]:
         if block["type"] == 0:
-            for page in block["lines"]:
-                for span in page["spans"]:
-                    if i not in files[span["size"]]:
-                        files[span["size"]][i] = []
-                    files[span["size"]][i].append(span["text"])
+            for line_num, line in enumerate(block["lines"]):
+                for span in line["spans"]:
+                    obj_key = f'{span["size"]}-{span["font"]}'
+                    if obj_key not in files:
+                        files[obj_key] = {}
+                    if page_num not in files[obj_key]:
+                        files[obj_key][page_num] = {}
+                    if line_num not in files[obj_key][page_num]:
+                        files[obj_key][page_num][line_num] = []
+                    if(span["text"].strip() != ''):
+                        files[obj_key][page_num][line_num].append(span["text"])
 
-for key, value in files.items():
-    print(key, len(value))
+line_counter = defaultdict(int)
+for key, file_contents in files.items():
     file_name = f"jen_{key}.txt"
     with open(Path(data_dir, file_name), 'w', encoding='utf-8') as file:
-        for page in value:
-            file.write(f"\n{page+1} page :\n")
-            for line in value[page]:
-                file.write(line)
-                file.write(' ')
+        for page_num, page_contents in file_contents.items():
+            file.write(f"\n-- {page_num+1} p --")
+            for line_num, line in page_contents.items():
+                file.write(f"\n{line_num+1} l:")
+                line_counter[key] += 1
+                for text in line:
+                    file.write(text)
+                    file.write(' ')
+
+line_count = sorted(line_counter.items(), key=lambda x: x[1], reverse=True)
+print(*map(lambda x: x[0], line_count), sep='\n')
+print(*map(lambda x: x[1], line_count), sep='\n')
